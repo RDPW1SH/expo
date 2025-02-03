79ff98c084ad47f89f93ed130aef71750a9eb986
RDPW1SH <rafael10ambrosio@gmail.com>
2025-02-03 10:41:04 +0000

    setup react-navigation

---
+++ teste/app/_layout.jsx
@@ -0,0 +1,11 @@
+import { NavigationContainer } from "@react-navigation/native";
+
+function _layout() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
