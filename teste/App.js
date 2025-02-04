20736929b35245d6416d5ba09571710f19d444ba
RDPW1SH <rafael10ambrosio@gmail.com>
2025-02-04 10:21:26 +0000

    link error

--- teste/App.js
+++ teste/App.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'react-native';
-import HomeScreen from './screens/Home';
+import HomeScreen from './screens/HomeScreen';
 import CategoryScreen from './screens/Category';
 import RecipeScreen from './screens/Recipe';
 
