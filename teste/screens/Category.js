6a8154833522df68aedaa76ef0a3cb93bfc95750
PedroFerreira6 <148778721+PedroFerreira6@users.noreply.github.com>
2025-02-04 09:43:33 +0000

    ...

--- teste/screens/Category.js
+++ teste/screens/Category.js
@@ -1,62 +1,51 @@
 import { Text, StyleSheet, FlatList, View, Image, ActivityIndicator, Pressable } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import CategoryComponent from '../components/CategoryComponente';
 
 const CategoryScreen = ({ route, navigation }) => {
 
     const [meals, setMeals] = useState([]);
     const [loading, setLoading] = useState(true);
-    
-    let categoryId = route?.params?.categoryId; 
+
+    let categoryId = route?.params?.categoryId;
 
     useEffect(() => {
         async function handleData() {
-    
-        try {
 
-        const res = await fetch('https://67a0e0ad5bcfff4fabe0f261.mockapi.io/api/testes/meals');
-            if(res.ok) {
-              const data = await res.json();
-              const filteredMeals = data.filter(meal => meal.categoryIds.includes(categoryId));
-              setMeals(filteredMeals);
+            try {
+
+                const res = await fetch('https://67a0e0ad5bcfff4fabe0f261.mockapi.io/api/testes/meals');
+                if (res.ok) {
+                    const data = await res.json();
+                    const filteredMeals = data.filter(meal => meal.categoryIds.includes(categoryId));
+                    setMeals(filteredMeals);
+                }
+            } catch (e) {
+                console.error(e)
             }
-        } catch (e) {
-            console.error(e)
-        }     
-            setLoading(false); 
+            setLoading(false);
         }
-        navigation.setOptions({title: route?.params?.categoryTitle || 'Categoria'});
+        navigation.setOptions({ title: route?.params?.categoryTitle || 'Categoria' });
         handleData()
     }, [categoryId])
 
-    if(loading) {
+    if (loading) {
         return (
-          <SafeAreaProvider>
-            <SafeAreaView style={styles.container}>
-              <ActivityIndicator size="large" color='#fff'/>
-            </SafeAreaView>
-          </SafeAreaProvider>
+            <SafeAreaProvider>
+                <SafeAreaView style={styles.container}>
+                    <ActivityIndicator size="large" color='#fff' />
+                </SafeAreaView>
+            </SafeAreaProvider>
         )
     }
 
-    
-    const renderItem = ({ item }) => (
-        <View style={[styles.listContainer, styles.shadowProp]}>
-            <Pressable
-                onPress={() => navigation.navigate('Recipe', { mealId: item.id })}
-            >
-                <Image style={styles.listImage} source={{ uri: item.imageUrl }} />
-                <View style={styles.listView}>
-                    <Text style={styles.listTitle}>{item.title}</Text>
-                    <View style={styles.listTextView}>
-                        <Text style={styles.listText}>{item.duration} Minutes</Text>
-                        <Text style={styles.listText}>{item.complexity}</Text>
-                        <Text style={styles.listText}>{item.affordability}</Text>
-                    </View>
-                </View>
-            </Pressable>
-        </View>
-    );
+
+    const renderItem = ( itemData ) => {
+        const item = itemData.item
+        const mealItemProps={id: item.id, title: item.title, imageUrl: item.imageUrl, duration: item.duration, complexity:item.complexity,affordability:item.affordability};
+        return <CategoryComponent {...mealItemProps} />
+    };
 
     return (
         <SafeAreaProvider>
@@ -80,42 +69,5 @@ const styles = StyleSheet.create({
         backgroundColor: '#A48686',
         paddingHorizontal: 25,
         paddingTop: 20,
-    },
-    listContainer: {
-        width: '100%',
-        borderRadius: 10,
-        backgroundColor: 'white',
-        marginBottom: 15,
-        overflow: 'hidden',
-    },
-    listView: {
-        padding: 15,
-        alignItems: 'center',
-    },
-    listImage: {
-        width: '100%',
-        height: 220,
-    },
-    listTextView: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        width: '100%',
-        marginTop: 10,
-    },
-    listTitle: {
-        fontSize: 20,
-        fontWeight: '600',
-        textAlign: 'center',
-        paddingBottom: 10,
-    },
-    listText: {
-        fontSize: 14,
-    },
-    shadowProp: {
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 4,
-        elevation: 4,
     }
 });
