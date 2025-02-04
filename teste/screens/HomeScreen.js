d09569e0a81e9b4a7c271699a382985a528ca0f1
RDPW1SH <rafael10ambrosio@gmail.com>
2025-02-04 10:07:05 +0000

    update home & components

--- teste/screens/HomeScreen.js
+++ teste/screens/HomeScreen.js
@@ -1,16 +1,8 @@
-import { View, Text, FlatList, Pressable, StyleSheet, ActivityIndicator } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { View, FlatList, StyleSheet } from 'react-native';
 import { useState, useEffect } from 'react';
+import LoadingComponent from '../components/LoadingComponent';
+import HomeCategoriesComponent from '../components/HomeCategoriesComponent';
 
-
-const Item = ({ id, title, color, navigation }) => (
-  <Pressable
-    style={[styles.item, { backgroundColor: color }]}
-    onPress={() => navigation.navigate('Category', {categoryId: id, categoryTitle: title})}
-  >
-    <Text style={styles.title}>{title}</Text>
-  </Pressable>
-);
 
 
 function HomeScreen({ navigation }) {
@@ -21,9 +13,9 @@ function HomeScreen({ navigation }) {
 
   useEffect(() => {
     async function handleData() {
-
       try {
         const res = await fetch('https://67a0e0ad5bcfff4fabe0f261.mockapi.io/api/testes/Category');
+
         if(res.ok) {
           const data = await res.json();
           setCategories(data);
@@ -31,8 +23,10 @@ function HomeScreen({ navigation }) {
 
       } catch (e) {
         console.error(e)
-      }     
-      setLoading(false); 
+      } finally {
+        setLoading(false); 
+      }  
+      
     }
     navigation.setOptions({title: 'All Categories'})
     handleData()
@@ -41,20 +35,17 @@ function HomeScreen({ navigation }) {
 
   if(loading) {
     return (
-      <SafeAreaProvider>
-        <SafeAreaView style={styles.container}>
-          <ActivityIndicator size="large" color='#fff'/>
-        </SafeAreaView>
-      </SafeAreaProvider>
+      <LoadingComponent />
     )
   }
+  
   return (
     <View style={styles.container}>
       <FlatList
         numColumns={2}
         data={categories}
         renderItem={({ item }) =>
-          <Item
+          <HomeCategoriesComponent
             id={item.id}
             title={item.title}
             color={item.color}
@@ -70,22 +61,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1, alignItems: 'center', justifyContent: 'center',
     backgroundColor: '#A48686',
-  },
-  title: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: '#000', 
-    textAlign: 'center',
-  },
-  item: {
-    backgroundColor: '#fff',
-    borderRadius: 10, // Bordas arredondadas
-    padding: 5, 
-    height:150,
-    width:150,
-    margin: 10, // Margem de 10px entre os itens
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 })
 
