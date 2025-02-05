f0d6cc6872c54a67a74d4eba709dffadd0132c79
RDPW1SH <rafael10ambrosio@gmail.com>
2025-02-05 18:14:49 +0000

    update addRecipe

--- teste/screens/AddRecipeScreen.js
+++ teste/screens/AddRecipeScreen.js
@@ -4,20 +4,28 @@ import {
   TextInput,
   StyleSheet,
   Pressable,
-  Button,
   Switch,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 import LoadingComponent from "../components/LoadingComponent";
+import { MultiSelect } from "react-native-element-dropdown";
+import Icon from "../components/IconComponent";
 
 const AddRecipeScreen = ({ navigation }) => {
+  const [categories, setCategories] = useState([]);
+  const [selectedCategories, setSelectedCategories] = useState([]);
   const [recipe, setRecipe] = useState({
     categoryIds: "",
     title: "",
     imageUrl: "",
-    ingredients: [""],
-    steps: [""],
+    ingredients: "",
+    steps: "",
     duration: "",
     complexity: "",
     affordability: "",
@@ -26,14 +34,15 @@ const AddRecipeScreen = ({ navigation }) => {
     isVegetarian: false,
     isLactoseFree: false,
   });
+  const [ingredients, setIngredients] = useState([""]);
+  const [steps, setSteps] = useState([""]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function handleData() {
       try {
         const res = await fetch(
-          //"https://67a0e0ad5bcfff4fabe0f261.mockapi.io/api/testes/Category"
-          "https://localhost:7199/api/category/listar-categorias"
+          "https://67a0e0ad5bcfff4fabe0f261.mockapi.io/api/testes/Category"
         );
 
         if (res.ok) {
@@ -46,67 +55,227 @@ const AddRecipeScreen = ({ navigation }) => {
         setLoading(false);
       }
     }
-    navigation.setOptions({ title: "All Categories" });
     handleData();
+  }, []);
+
+  useEffect(() => {
+    navigation.setOptions({ title: "All Categories" });
   }, [navigation]);
+
+  const handleInputChange = (key, value) => {
+    setRecipe((previousState) => ({
+      ...previousState,
+      [key]: value,
+    }));
+  };
 
   if (loading) {
     return <LoadingComponent />;
   }
 
-  function handleNewRecipe() {}
+  const handleAddIngredient = () => {
+    setIngredients([...ingredients, ""]);
+  };
+
+  const handleRemoveIngredient = (index) => {
+    setIngredients(ingredients.filter((_, i) => i !== index));
+  };
+
+  const handleIngredientChange = (text, index) => {
+    const newIngredients = [...ingredients];
+    newIngredients[index] = text;
+    setIngredients(newIngredients);
+  };
+
+  const handleAddStep = () => {
+    setSteps([...steps, ""]);
+  };
+
+  const handleRemoveStep = (index) => {
+    setSteps(steps.filter((_, i) => i !== index));
+  };
+
+  const handleStepChange = (text, index) => {
+    const newSteps = [...steps];
+    newSteps[index] = text;
+    setSteps(newSteps);
+  };
+
+  const handleNewRecipe = () => {
+    const formattedCategories = selectedCategories.join(";");
+    const formattedIngredients = ingredients.join(";");
+    const formattedSteps = steps.join(";");
+    const recipeTitle =
+      recipe.title.charAt(0).toUpperCase() + recipe.title.slice(1);
+    const recipeComplexity =
+      recipe.complexity.charAt(0).toUpperCase() + recipe.complexity.slice(1);
+    const recipeAffordability =
+      recipe.affordability.charAt(0).toUpperCase() +
+      recipe.affordability.slice(1);
+
+    const newRecipe = {
+      categoryIds: formattedCategories,
+      title: recipeTitle,
+      imageUrl: recipe.imageUrl,
+      ingredients: formattedIngredients,
+      steps: formattedSteps,
+      duration: recipe.duration,
+      complexity: recipeComplexity,
+      affordability: recipeAffordability,
+      isGlutenFree: recipe.isGlutenFree,
+      isVegan: recipe.isVegan,
+      isVegetarian: recipe.isVegetarian,
+      isLactoseFree: recipe.isLactoseFree,
+    };
+
+    console.log(newRecipe);
+  };
+
   return (
-    <SafeAreaProvider style={styles.container}>
-      <SafeAreaView style={styles.form}>
-        <View>
-          <Text>Nome da receita</Text>
-          <TextInput />
-        </View>
-        <View>
-          <Text>URL da imagem</Text>
-          <TextInput />
-        </View>
-        <View>
-          <Text>Ingredientes</Text>
-        </View>
-        <View>
-          <Text>Passos</Text>
-        </View>
-        <View>
-          <Text>Duração (em minutos)</Text>
-          <TextInput />
-        </View>
-        <View>
-          <Text>Custo</Text>
-          <TextInput />
-        </View>
-        <View>
-          <Text>Dificuldade</Text>
-          <TextInput />
-        </View>
-        <View>
-          <View>
-            <Text>Contem Gluten?</Text>
-            <Switch />
+    <SafeAreaProvider style={styles.default}>
+      <SafeAreaView style={styles.container}>
+        <ScrollView style={{ width: "90%" }}>
+          <View style={styles.form}>
+            <View>
+              <Text style={styles.label}>Nome da receita</Text>
+              <TextInput
+                style={styles.formTextInput}
+                onChangeText={(text) => handleInputChange("title", text)}
+              />
+            </View>
+            <View>
+              <Text style={styles.label}>URL da imagem</Text>
+              <TextInput
+                style={styles.formTextInput}
+                onChangeText={(text) => handleInputChange("imageUrl", text)}
+              />
+            </View>
+            <View>
+              <Text style={styles.label}>Categorias</Text>
+              <MultiSelect
+                data={categories}
+                labelField="title"
+                valueField="id"
+                placeholder="Select Categories"
+                value={selectedCategories}
+                onChange={(items) => setSelectedCategories(items)}
+                style={styles.picker}
+              />
+            </View>
+            <View>
+              <Text style={styles.label}>Ingredientes</Text>
+              {ingredients.map((ingredient, index) => (
+                <View key={index} style={styles.ingredientContainer}>
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Enter ingredient"
+                    value={ingredient}
+                    onChangeText={(text) => handleIngredientChange(text, index)}
+                  />
+                  <Pressable onPress={() => handleRemoveIngredient(index)}>
+                    <Icon name={"remove"} size={24} color="black" />
+                  </Pressable>
+                </View>
+              ))}
+              <Pressable onPress={handleAddIngredient} style={styles.addButton}>
+                <Text style={styles.addButtonText}>Adicionar ingrediente</Text>
+              </Pressable>
+            </View>
+            <View>
+              <Text style={styles.label}>Passos</Text>
+              {steps.map((step, index) => (
+                <View key={index} style={styles.ingredientContainer}>
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Enter steps"
+                    value={step}
+                    onChangeText={(text) => handleStepChange(text, index)}
+                  />
+                  <Pressable onPress={() => handleRemoveStep(index)}>
+                    <Icon name={"remove"} size={24} color="black" />
+                  </Pressable>
+                </View>
+              ))}
+              <Pressable onPress={handleAddStep} style={styles.addButton}>
+                <Text style={styles.addButtonText}>Adicionar Passos</Text>
+              </Pressable>
+            </View>
+            <View>
+              <Text style={styles.label}>Duração (em minutos)</Text>
+              <TextInput
+                style={styles.formTextInput}
+                onChangeText={(text) => handleInputChange("duration", text)}
+              />
+            </View>
+            <View>
+              <Text style={styles.label}>Custo</Text>
+              <TextInput
+                style={styles.formTextInput}
+                onChangeText={(text) =>
+                  handleInputChange("affordability", text)
+                }
+              />
+            </View>
+            <View>
+              <Text style={styles.label}>Dificuldade</Text>
+              <TextInput
+                style={styles.formTextInput}
+                onChangeText={(text) => handleInputChange("complexity", text)}
+              />
+            </View>
+            <View>
+              <Text style={styles.label}>Contem Gluten?</Text>
+              <Switch
+                trackColor={{ false: "#767577", true: "#81b0ff" }}
+                thumbColor={recipe.isGlutenFree ? "#f5dd4b" : "#f4f3f4"}
+                ios_backgroundColor="#3e3e3e"
+                value={recipe.isGlutenFree}
+                onValueChange={(newValue) =>
+                  handleInputChange("isGlutenFree", newValue)
+                }
+              />
+            </View>
+            <View>
+              <Text style={styles.label}>É Vegan?</Text>
+              <Switch
+                trackColor={{ false: "#767577", true: "#81b0ff" }}
+                thumbColor={recipe.isVegan ? "#f5dd4b" : "#f4f3f4"}
+                ios_backgroundColor="#3e3e3e"
+                value={recipe.isVegan}
+                onValueChange={(newValue) =>
+                  handleInputChange("isVegan", newValue)
+                }
+              />
+            </View>
+            <View>
+              <Text style={styles.label}>É Vegetariana?</Text>
+              <Switch
+                trackColor={{ false: "#767577", true: "#81b0ff" }}
+                thumbColor={recipe.isVegetarian ? "#f5dd4b" : "#f4f3f4"}
+                ios_backgroundColor="#3e3e3e"
+                value={recipe.isVegetarian}
+                onValueChange={(newValue) =>
+                  handleInputChange("isVegetarian", newValue)
+                }
+              />
+            </View>
+            <View>
+              <Text style={styles.label}>Contem Lactose?</Text>
+              <Switch
+                trackColor={{ false: "#767577", true: "#81b0ff" }}
+                thumbColor={recipe.isLactoseFree ? "#f5dd4b" : "#f4f3f4"}
+                ios_backgroundColor="#3e3e3e"
+                value={recipe.isLactoseFree}
+                onValueChange={(newValue) =>
+                  handleInputChange("isLactoseFree", newValue)
+                }
+              />
+            </View>
+            <Pressable style={styles.formPressable} onPress={handleNewRecipe}>
+              <Text style={styles.formPressableText}>Criar Receita</Text>
+            </Pressable>
           </View>
-          <View>
-            <Text>É Vegan?</Text>
-            <Switch />
-          </View>
-          <View>
-            <Text>É Vegetariana?</Text>
-            <Switch />
-          </View>
-          <View>
-            <Text>Contem Lactose?</Text>
-            <Switch />
-          </View>
-        </View>
-        <View>
-          <Pressable onPress={handleNewRecipe}>
-            <Text>Criar Receita</Text>
-          </Pressable>
-        </View>
+        </ScrollView>
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -118,11 +287,69 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#A48686",
     paddingHorizontal: 25,
-    paddingTop: 20,
+    paddingVertical: 20,
   },
   form: {
+    padding: 20,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+  },
+  formTextInput: {
+    height: 50,
+    width: "100%",
+    backgroundColor: "#f9f9f9",
+    borderRadius: 8,
+    marginBottom: 10,
+    paddingLeft: 15,
+  },
+  label: {
+    fontSize: 16,
+    paddingBottom: 2,
+    fontWeight: "600",
+  },
+  picker: {
+    height: 50,
+    width: "100%",
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    paddingLeft: 15,
+  },
+  input: {
+    height: 50,
+    width: "80%",
+    backgroundColor: "#f9f9f9",
+    borderRadius: 8,
+    marginBottom: 10,
+    paddingLeft: 15,
+  },
+  ingredientContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 10,
+  },
+  addButton: {
+    marginVertical: 10,
     padding: 10,
-    backgroundColor: "white",
+    backgroundColor: "lightblue",
+    borderRadius: 8,
+  },
+  addButtonText: {
+    textAlign: "center",
+  },
+  formPressable: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "auto",
+    backgroundColor: "lightblue",
+    padding: 15,
+    borderRadius: 8,
+    marginTop: 20,
+  },
+  formPressableText: {
+    textAlign: "center",
+    fontSize: 18,
   },
 });
+
 export default AddRecipeScreen;
