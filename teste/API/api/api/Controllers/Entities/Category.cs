bd1f3563d3f68f20aab8a52f46af6d07e54a76a7
RDPW1SH <rafael10ambrosio@gmail.com>
2025-02-06 11:29:42 +0000

    Atualizando .gitignore para ignorar arquivos temporários

--- teste/API/api/api/Controllers/Entities/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace API.Controllers.Entities
-{
-    public class Category
-    {
-        public long Id { get; set; }
-        public required string Title { get; set; }
-        public string Color { get; set; }
-    }
-}
--- teste/components/IconComponent.js
+++
@@ -1,8 +0,0 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-
-export default function Icon({name, size, color}) {
-
-    return (
-        <FontAwesome style={{cursor: 'pointer'}} name={name} size={size} color={color} />
-    )
-}
