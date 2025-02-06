fa4bd24677fa08feea47008c9afd301498a8cb21
PedroFerreira6 <148778721+PedroFerreira6@users.noreply.github.com>
2025-02-06 17:46:29 +0000

    ..

--- teste/API/api/api/Controllers/Entities/Meal.cs
+++ teste/API/api/api/Controllers/Entities/Meal.cs
@@ -15,6 +15,26 @@
         public bool IsVegan { get; set; }
         public bool IsVegetarian { get; set; }
         public bool IsLactoseFree { get; set; }
+
+
+
+
+    }
+    public class AddMeal
+    {
+        public long Id { get; set; }
+        public List<long> CategoryIds { get; set; } = new List<long>();
+        public required string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public List<string> Ingredients { get; set; } = new List<string>();
+        public List<string> Steps { get; set; } = new List<string>();
+        public int Duration { get; set; }
+        public string Complexity { get; set; }
+        public string Affordability { get; set; }
+        public bool IsGlutenFree { get; set; }
+        public bool IsVegan { get; set; }
+        public bool IsVegetarian { get; set; }
+        public bool IsLactoseFree { get; set; }
     }
 
 }
